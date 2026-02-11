550f1c7bb9ccf13b3974be8ac8eddad34c98fa50
eunjin050110-rgb <eunjin050110@gmail.com>
2026-02-12 00:18:58 +0900

    feat[BOJ] 2863.cpp

---
+++ PS/BOJ/01000-10000/02001-03000/2863.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    double a[5];
+    double ans[4];
+    for (int i = 1; i <= 4; i++)
+        cin >> a[i];
+    ans[0] = a[1] / a[3] + a[2] / a[4];
+    ans[1] = a[3] / a[4] + a[1] / a[2];
+    ans[2] = a[4] / a[2] + a[3] / a[1];
+    ans[3] = a[2] / a[1] + a[4] / a[3];
+    double rot = 0;
+    int mini = 10;
+
+    for(int i = 0; i < 4; i++)
+       if (rot < ans[i])
+           rot = ans[i];
+
+    for (int i = 0; i < 4; i++)
+        if (rot == ans[i])
+        mini = min(mini, i);
+    cout << mini << '\n';
+}
