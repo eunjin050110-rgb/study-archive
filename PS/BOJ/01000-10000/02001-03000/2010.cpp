8f813a6b9150df29abda59472225615ac66e1a05
eunjin050110-rgb <eunjin050110@gmail.com>
2026-02-22 23:31:12 +0900

    feat[BOJ] 2010.cpp

---
+++ PS/BOJ/01000-10000/02001-03000/2010.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int con;
+        cin >> con;
+        sum += con;
+    }
+    sum -= (n - 1);
+    cout << sum << '\n';
+    return 0;
+}
